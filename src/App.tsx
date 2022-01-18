937efdca7c4c26458ed7d5a5f2cc1308d3babc36
ppesterev <ppesterev@mail.ru>
2022-01-19 04:00:26 +0700

    try rendering data loaded from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import { fetchContacts } from "./api";
+import { loadContacts } from "./store/contacts/contactsSlice";
+import { useAppSelector, useAppDispatch } from "./store/hooks";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const dispatch = useAppDispatch();
+  const contacts = useAppSelector((state) => state.contacts);
+
+  useEffect(() => {
+    fetchContacts().then((contacts) => {
+      dispatch(loadContacts(contacts));
+    });
+  }, []);
 
   return (
     <div className="App">
       <h1>My Contacts</h1>
+      {contacts && (
+        <ul>
+          {contacts.map((contact) => (
+            <li key={contact.id}>{contact.name}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
