df881c4f3b10482ceddf7db1a4495a75f49a6da6
ppesterev <ppesterev@mail.ru>
2022-01-24 00:16:39 +0700

    define selectors

---
+++ src/store/selectors.ts
@@ -0,0 +1,50 @@
+import { createSelector } from "@reduxjs/toolkit";
+
+import { RootState } from ".";
+
+const getContacts = (state: RootState) => state.contacts;
+const getFilters = (state: RootState) => state.filters;
+const getSorting = (state: RootState) => state.sorting;
+
+const getFilteredContacts = createSelector(
+  [getContacts, getFilters],
+  (records, { search, onlyFavorites }) => {
+    if (!records) {
+      return records;
+    }
+    return records.filter((record) => {
+      const nameMatches = record.contact.name
+        .toLowerCase()
+        .includes(search.toLowerCase());
+
+      return nameMatches && (!onlyFavorites || record.isFavorite);
+    });
+  }
+);
+
+const getContactsToDisplay = createSelector(
+  [getFilteredContacts, getSorting],
+  (records, { sortedBy, isDescending }) => {
+    if (!records) {
+      return records;
+    }
+    return records?.sort((a, b) => {
+      const [nameA, nameB] = [
+        a.contact.name.toLowerCase(),
+        b.contact.name.toLowerCase()
+      ];
+      let order = 0;
+      switch (sortedBy) {
+        case "name":
+          order = nameA < nameB ? -1 : nameA > nameB ? 1 : 0;
+          break;
+
+        case "date":
+          order = a.modifiedTimestamp - b.modifiedTimestamp;
+      }
+      return isDescending ? -order : order;
+    });
+  }
+);
+
+export { getContacts, getFilters, getSorting, getContactsToDisplay };
