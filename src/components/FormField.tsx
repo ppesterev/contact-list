c54add7064574cd7a8db66b510c6e553c90ffc6d
ppesterev <ppesterev@mail.ru>
2022-01-24 21:57:05 +0700

    spread input attributes to the input in FormField

--- src/components/FormField.tsx
+++ src/components/FormField.tsx
@@ -8,14 +8,25 @@ interface Props extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
 }
 
-export default function FormField({ name, label }: Props) {
+export default function FormField({
+  name,
+  label,
+  color,
+  unselectable,
+  size,
+  ...rest
+}: Props) {
   const [input, meta, helpers] = useField(name);
 
   return (
     <Form.Field>
       {label && <Form.Label>{label}</Form.Label>}
       <Form.Control>
-        <Form.Input {...input} />
+        <Form.Input
+          {...rest}
+          {...input}
+          color={meta.touched ? (meta.error ? "danger" : "success") : undefined}
+        />
       </Form.Control>
       {meta.error && meta.touched && <Form.Help>{meta.error}</Form.Help>}
     </Form.Field>
