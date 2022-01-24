cd441ebdd783aee819e159b84fc41b9f329a68ea
ppesterev <ppesterev@mail.ru>
2022-01-24 13:49:24 +0700

    add component to group list items alphabetically

---
+++ src/components/AlphabeticGrouping.tsx
@@ -0,0 +1,56 @@
+import { useMemo, ReactNode, Fragment } from "react";
+
+interface Props<T> {
+  items: T[];
+  alphabetization: (item: T) => string;
+  children: (groupItems: T[]) => ReactNode;
+}
+
+interface Group<T> {
+  initial: string | null;
+  items: T[];
+}
+
+export default function AlphabeticGrouping<T>({
+  items,
+  alphabetization,
+  children
+}: Props<T>) {
+  const groups: Group<T>[] = useMemo(() => {
+    const groups: Group<T>[] = [];
+
+    const alphabetizedItems = items.slice().sort((a, b) => {
+      const [stringA, stringB] = [a, b].map((item) =>
+        alphabetization(item).toLowerCase()
+      );
+      return stringA < stringB ? -1 : stringA > stringB ? 1 : 0;
+    });
+
+    for (const item of alphabetizedItems) {
+      const itemInitial = alphabetization(item)[0].toUpperCase() || null;
+      let group: Group<T> = groups[groups.length - 1];
+
+      if (!group || group.initial !== itemInitial) {
+        // start new group
+        group = { initial: itemInitial?.toUpperCase() || null, items: [item] };
+        groups.push(group);
+      } else {
+        // add to existing group
+        group.items.push(item);
+      }
+    }
+
+    return groups;
+  }, [items, alphabetization]);
+
+  return (
+    <>
+      {groups.map((group) => (
+        <Fragment key={group.initial}>
+          <h2>{group.initial}</h2>
+          {children(group.items)}
+        </Fragment>
+      ))}
+    </>
+  );
+}
