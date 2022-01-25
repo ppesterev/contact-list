988bbc84ae0ea7b305e28aab6087d623d156efa4
ppesterev <ppesterev@mail.ru>
2022-01-25 18:50:21 +0700

    contact card layout & buttons

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
+
 import { useAppDispatch } from "../store/hooks";
 
+import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Link from "@material-ui/core/Link";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import Collapse from "@material-ui/core/Collapse";
 
 import Phone from "@material-ui/icons/Phone";
 import Public from "@material-ui/icons/Public";
 import Mail from "@material-ui/icons/Mail";
+import Bookmark from "@material-ui/icons/Bookmark";
+import BookmarkBorder from "@material-ui/icons/BookmarkBorder";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
 
 import {
   setFavorite,
@@ -25,6 +35,9 @@ interface Props {
 }
 
 const useStyles = makeStyles((theme) => ({
+  card: {
+    maxWidth: "400px"
+  },
   icon: {
     fill: theme.palette.text.secondary,
     marginRight: theme.spacing(1),
@@ -44,44 +57,75 @@ export default function ContactCard({ record, onEditContact }: Props) {
     isFavorite
   } = record;
 
+  const [isExpanded, setIsExpanded] = useState(false);
+
   return (
-    <article>
-      <Typography variant="body1" component={ContextualHeading} gutterBottom>
-        {name}{" "}
-        <Typography variant="body1" component="span" color="textSecondary">
-          ({username})
-        </Typography>
-      </Typography>
-      <HeadingRegion>
-        <Typography variant="body2" gutterBottom>
-          <Mail className={classes.icon} />
-          <Link href={`mailto:${email}`}>{email}</Link>
-        </Typography>
-        {phone && (
-          <Typography variant="body2" gutterBottom>
-            <Link href={`tel:${phone}`}>
-              <Phone className={classes.icon} />
-              {phone}
-            </Link>
+    <article className={classes.card}>
+      <Grid
+        container
+        justifyContent="space-between"
+        alignItems="flex-start"
+        wrap="nowrap"
+        component="header"
+      >
+        <Typography variant="body1" component={ContextualHeading} gutterBottom>
+          {name}{" "}
+          <Typography variant="body1" component="span" color="textSecondary">
+            ({username})
           </Typography>
-        )}
-        {website && (
-          <Typography variant="body2" gutterBottom>
-            <Link href={website}>
-              <Public className={classes.icon} />
-              {website}
-            </Link>
-          </Typography>
-        )}
-        <button onClick={() => onEditContact(id)}>Edit contact</button>
-        <button
+        </Typography>
+        <IconButton
+          size="small"
+          color={isFavorite ? "primary" : "default"}
           onClick={() => dispatch(setFavorite({ id, isFavorite: !isFavorite }))}
+          aria-label={isFavorite ? "unfavorite" : "favorite"}
         >
-          {isFavorite ? "Remove from favorites" : "Add to favorites"}
-        </button>
-        <button onClick={() => dispatch(deleteContact(id))}>
-          Delete contact
-        </button>
+          {isFavorite ? <Bookmark /> : <BookmarkBorder />}
+        </IconButton>
+      </Grid>
+
+      <HeadingRegion>
+        <Grid container justifyContent="space-between" alignItems="flex-end">
+          <Grid item>
+            <Typography variant="body2" gutterBottom>
+              <Mail className={classes.icon} />
+              <Link href={`mailto:${email}`}>{email}</Link>
+            </Typography>
+            {phone && (
+              <Typography variant="body2" gutterBottom>
+                <Link href={`tel:${phone}`}>
+                  <Phone className={classes.icon} />
+                  {phone}
+                </Link>
+              </Typography>
+            )}
+            {website && (
+              <Typography variant="body2" gutterBottom>
+                <Link href={website}>
+                  <Public className={classes.icon} />
+                  {website}
+                </Link>
+              </Typography>
+            )}
+          </Grid>
+          <Grid item>
+            <IconButton
+              onClick={() => setIsExpanded((is) => !is)}
+              size="small"
+              aria-label={isExpanded ? "more" : "less"}
+            >
+              {isExpanded ? <ExpandLess /> : <ExpandMore />}
+            </IconButton>
+          </Grid>
+        </Grid>
+        <Collapse in={isExpanded}>
+          <Button color="primary" onClick={() => onEditContact(id)}>
+            Edit
+          </Button>
+          <Button color="primary" onClick={() => dispatch(deleteContact(id))}>
+            Delete
+          </Button>
+        </Collapse>
       </HeadingRegion>
     </article>
   );
